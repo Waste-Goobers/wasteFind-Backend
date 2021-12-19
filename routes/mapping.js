e1cbe2f7cd6509d5c06fd6cbf61424bbe319dc0d
configtheworld <erkinture@gmail.com>
2021-12-20 00:38:55 +0300

    mapping routes and controllers defined

---
+++ routes/mapping.js
@@ -0,0 +1,44 @@
+var express = require('express');
+const axios = require('axios');
+const mappingController = require('../controllers/mapping.controller');
+var router = express.Router();
+
+/**
+ * Find Near Recycle Centers By Zipcode
+ * @body { "zipcode":"06370" }
+ * @returns  obj { recycle_centers:[] }
+ */
+router.post('/zipcode', (req, res, next) => {
+  try {
+    if (req.body.zipcode) {
+      res.send({
+        recycle_centers: mappingController.calculateByZipcode(req.body.zipcode),
+      });
+    }
+  } catch (e) {
+    res.status(406);
+    res.send('Wrong or Missing Zipcode Try again');
+  }
+});
+
+/**
+ * Find Near Recycle Centers By Location
+ * @body { "location":{ "lat": -33.8599358, "lng": 151.2090295 }}
+ * @returns arr { recycle_centers:[] }
+ */
+router.post('/location', (req, res, next) => {
+  try {
+    if (req.body.location) {
+      res.send({
+        recycle_centers: mappingController.calculateByLocation(
+          req.body.location
+        ),
+      });
+    }
+  } catch (e) {
+    res.status(406);
+    res.send('Wrong or Missing Zipcode Try again');
+  }
+});
+
+module.exports = router;
