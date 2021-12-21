9e50eaa582f31310cc9d8d19f0b474f50ded7b45
configtheworld <erkinture@gmail.com>
2021-12-21 05:17:03 +0300

    firebase configuration and user jwt middleware added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const middleware = require('../middleware/user.middleware');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/test-auth', middleware.decodeToken, function (req, res, next) {
+  res.send('This message required a token to see');
 });
 
 module.exports = router;
